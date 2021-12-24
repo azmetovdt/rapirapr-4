e9a02e9bb1a156ec10b1be911573f208d96c5fc1
Azmetov Danila <ohstapit@gmail.com>
2021-12-24 22:03:44 +0300

    Scripted auto-commit on change (2021-12-24 22:03:44) by gitwatch.sh

---
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/StoreActor.java
@@ -0,0 +1,36 @@
+package ru.bmstu.rapirapr.azmetov.akka;
+
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StoreActor extends AbstractActor {
+
+    private final Map<String, List<TestResult>> testResultsMap = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(TestResult.class, testResult -> saveResults(testResult.getMessageTest().getPackageId(), testResult))
+                .match(String.class, id -> sender().tell(getProgramResults(id), self()))
+                .build();
+    }
+
+    private void saveResults(String id, TestResult result) {
+        if (!testResultsMap.containsKey(id)) {
+            testResultsMap.put(id, new ArrayList<>());
+        }
+        testResultsMap.get(id).add(result);
+    }
+
+    private List<TestResult> getProgramResults(String id) {
+        if (!testResultsMap.containsKey(id)) {
+            return new ArrayList<>();
+        }
+        return testResultsMap.get(id);
+    }
+}
