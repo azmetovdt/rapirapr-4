26676b27167510ba1f9a9a009c1a9a930b8f200c
Azmetov Danila <ohstapit@gmail.com>
2021-12-24 21:25:32 +0300

    Scripted auto-commit on change (2021-12-24 21:25:32) by gitwatch.sh

---
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
@@ -0,0 +1,56 @@
+package ru.bmstu.rapirapr.azmetov.akka;
+
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import scala.concurrent.Future;
+
+import java.util.concurrent.CompletionStage;
+
+import static akka.http.javadsl.server.Directives.*;
+
+public class TesterApp {
+    public static final String ACTOR_SYSTEM_NAME = "TesterActorSystem";
+    public static final Integer HTTP_PORT = 8080;
+    public static final String HTTP_HOST = "localhost";
+    public static final String SERVER_STARTED_MESSAGE = "Сервер запущен";
+    public static final String TESTING_STARTED_RESPONSE = "Тестирование запущено";
+    public static final String PACKAGE_ID_PARAMETER_ALIAS = "packageId";
+
+    public static void main(String[] args) throws Exception {
+        ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        ActorRef actor = system.actorOf(Props.create(RouterActor.class));
+        ActorMaterializer materializer = ActorMaterializer.create(system);
+        TesterApp app = new TesterApp();
+        Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute(actor).flow(system, materializer);
+        Http http = Http.get(system);
+        CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost(HTTP_HOST, HTTP_PORT), materializer);
+        System.out.println(SERVER_STARTED_MESSAGE);
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
+    }
+
+    private Route createRoute(ActorRef actor) {
+        return route(
+                get(() -> parameter(PACKAGE_ID_PARAMETER_ALIAS, id -> {
+                    Future<Object> result = Patterns.ask(actor, id, 5000);
+                    return completeOKWithFuture(result, Jackson.marshaller());
+                })),
+                post(() -> entity(Jackson.unmarshaller(Message.class), order -> {
+                    actor.tell(order, ActorRef.noSender());
+                    return complete(TESTING_STARTED_RESPONSE);
+                }))
+        );
+    }
+}
