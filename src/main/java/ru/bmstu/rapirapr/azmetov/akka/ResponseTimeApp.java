1e0b67740bde0e6ba3e03d9a0270d167d574766d
Azmetov Danila <ohstapit@gmail.com>
2021-12-24 22:40:53 +0300

    Scripted auto-commit on change (2021-12-24 22:40:53) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
@@ -65,8 +65,7 @@ public class ResponseTimeApp {
                                     return CompletableFuture.completedFuture(result);
                                 }
                                 final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = Flow.<Pair<String, Integer>>create()
-                                                .mapConcat(_pair -> new ArrayList<>(Collections.nCopies(_pair.second(), _pair.first()));
-                                                })
+                                                .mapConcat(_pair -> new ArrayList<>(Collections.nCopies(_pair.second(), _pair.first())))
                                         .mapAsync(pair.second(),);
                             }
                     );
