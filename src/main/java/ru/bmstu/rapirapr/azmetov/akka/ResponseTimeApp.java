1b7bfa6837302bf408f7c44af1f713c18aa2c737
Azmetov Danila <ohstapit@gmail.com>
2021-12-24 23:13:43 +0300

    Scripted auto-commit on change (2021-12-24 23:13:43) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
@@ -79,6 +79,7 @@ public class ResponseTimeApp {
                             });
                 })
                 .map(request -> {
+                    System.out.println("Saving result");
                     
                 });
     });
