b6358788b4e3b7da369eb7fc06c921e1e75a5d81
Azmetov Danila <ohstapit@gmail.com>
2021-12-24 21:30:32 +0300

    Scripted auto-commit on change (2021-12-24 21:30:32) by gitwatch.sh

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Message {
+public class Message {
+    private String url;
+
+    public Message(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
