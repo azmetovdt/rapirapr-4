2bc7d2b723ddc83ace2460ffbab63b86f4a38696
Azmetov Danila <ohstapit@gmail.com>
2021-12-24 21:49:35 +0300

    Scripted auto-commit on change (2021-12-24 21:49:35) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
@@ -52,7 +52,7 @@ public class ResponseTimeApp {
         return Flow.of(HttpRequest.class)
                 .map(
                 (request) -> {
-                    CompletionStage<Object> = Patterns.ask(actor, );
+                    CompletionStage<Object> = Patterns.ask(actor, new Message());
                     final Query query = request.getUri().query();
 
                     return new Pair<>(
