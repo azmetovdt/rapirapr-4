71a40142f66bff1113e814da47ed3d427e0991e9
Azmetov Danila <ohstapit@gmail.com>
2021-12-24 22:48:59 +0300

    Scripted auto-commit on change (2021-12-24 22:48:59) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
@@ -71,7 +71,7 @@ public class ResponseTimeApp {
                                             System.out.println("Executing test");
                                             return CompletableFuture.completedFuture(0);
                                         });
-                                return Source.from(Collections.singletonList(r))
+                                return Source.
                                         .toMat(Sink.fold(0, Integer::sum), Keep.right()).run(materializer);
                             }
                     );
