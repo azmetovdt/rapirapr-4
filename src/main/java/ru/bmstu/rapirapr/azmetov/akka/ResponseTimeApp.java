79d2d8aaa5f38193d31a2879f971141332713dc5
Azmetov Danila <ohstapit@gmail.com>
2021-12-24 22:27:15 +0300

    Scripted auto-commit on change (2021-12-24 22:27:15) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
@@ -35,7 +35,7 @@ public class ResponseTimeApp {
         ActorMaterializer materializer = ActorMaterializer.create(system);
         ResponseTimeApp app = new ResponseTimeApp();
         final Http http = Http.get(system);
-        final Flow<HttpRequest, Object, NotUsed> routeFlow = createRoute(actor);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createRoute(actor);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost(HTTP_HOST, HTTP_PORT),
@@ -46,7 +46,7 @@ public class ResponseTimeApp {
         binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 
-    private static Flow<HttpRequest, Object, NotUsed> createRoute(ActorRef actor) {
+    private static Flow<HttpRequest, HttpResponse, NotUsed> createRoute(ActorRef actor) {
         return Flow.of(HttpRequest.class)
                 .map((request) -> {
                     final Query query = request.getUri().query();
@@ -65,7 +65,8 @@ public class ResponseTimeApp {
                                 return null;
                             }
                     );
-                });
+                })
+        return null;
     });
 
 
