67f95d12e576cce0ccf2fe8e40d640c8106de817
Azmetov Danila <ohstapit@gmail.com>
2021-12-25 11:45:43 +0300

    Scripted auto-commit on change (2021-12-25 11:45:43) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ResponseTimeApp.java
@@ -47,7 +47,7 @@ public class ResponseTimeApp {
         return route(
                 get(() -> parameter(URL_QUERY_PARAMETER_ALIAS, url ->
                         parameter(COUNT_QUERY_PARAMETER_ALIAS, count -> {
-                            if(count)
+                            if(Integer.parseInt(count) > 0)
                             Future<Object> result = Patterns.ask(actor, url, 5000);
                             return completeOKWithFuture(fetch(http, url));
                         })
