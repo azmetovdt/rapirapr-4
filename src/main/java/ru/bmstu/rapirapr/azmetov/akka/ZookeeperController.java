5498c04c48aa0807a0cbdf1b141b01259699c12b
Azmetov Danila <ohstapit@gmail.com>
2021-12-25 12:45:07 +0300

    Scripted auto-commit on change (2021-12-25 12:45:07) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/ZookeeperController.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ZookeeperController.java
@@ -1,7 +1,9 @@
 package ru.bmstu.rapirapr.azmetov.akka;
 
+import akka.actor.ActorRef;
 import org.apache.zookeeper.ZooKeeper;
 
 public class ZookeeperController {
     private ZooKeeper zoo;
+    private ActorRef
 }
