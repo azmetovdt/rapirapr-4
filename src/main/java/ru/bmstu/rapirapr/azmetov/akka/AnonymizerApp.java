531cc522aac0e22d993cbd06f92819c0edb064e6
Azmetov Danila <ohstapit@gmail.com>
2021-12-25 13:23:24 +0300

    Scripted auto-commit on change (2021-12-25 13:23:24) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/AnonymizerApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/AnonymizerApp.java
@@ -40,7 +40,7 @@ public class AnonymizerApp {
                 materializer
         );
         final ZookeeperConfiguration controller = new ZookeeperConfiguration(ZOOKEEPER_HOST, actor);
-        controller.addServerNode();
+        controller.addServerNode(joinUrl());
         System.out.println(SERVER_STARTED_MESSAGE);
         System.in.read();
         binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
