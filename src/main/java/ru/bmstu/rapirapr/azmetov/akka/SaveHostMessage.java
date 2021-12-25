38a8c242c1ec1685938f7dd100b722e9e1cf52bf
Azmetov Danila <ohstapit@gmail.com>
2021-12-25 12:15:48 +0300

    Scripted auto-commit on change (2021-12-25 12:15:48) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/SaveHostMessage.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/SaveHostMessage.java
@@ -2,5 +2,13 @@ package ru.bmstu.rapirapr.azmetov.akka;
 
 
 public class SaveHostMessage {
+    private static String host;
 
+    public SaveHostMessage(String host) {
+        this.host = host;
+    }
+
+    public String getHost() {
+        return host;
+    }
 }
