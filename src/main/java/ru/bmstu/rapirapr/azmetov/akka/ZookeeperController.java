c77745eac483e2abb69bbec736358a8f06dfff71
Azmetov Danila <ohstapit@gmail.com>
2021-12-25 13:14:46 +0300

    Scripted auto-commit on change (2021-12-25 13:14:46) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/ZookeeperController.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ZookeeperController.java
@@ -15,6 +15,7 @@ public class ZookeeperController {
     public static final String NODE_ROOT_PATH = "/s";
     public static final String NODE_SERVER_PATH = "/server";
     public static final String UPDATE_LOG_MESSAGE = "Список нод изменилсяо: ";
+    public static final Integer 
 
     public ZookeeperController(String host, ActorRef actor) {
         this.actor = actor;
