6a352e27c86aa7f46091d11cf6bcdac7f463f7e4
Azmetov Danila <ohstapit@gmail.com>
2021-12-25 12:00:38 +0300

    Scripted auto-commit on change (2021-12-25 12:00:38) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/Message.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/Message.java
@@ -2,13 +2,4 @@ package ru.bmstu.rapirapr.azmetov.akka;
 
 
 public class RandomHostMessage {
-    private final String url;
-
-    public Message(String url) {
-        this.url = url;
-    }
-
-    public String getUrl() {
-        return url;
-    }
 }
