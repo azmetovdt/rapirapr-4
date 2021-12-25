c638017c3c6520ef6ff166607575a047b46a0560
Azmetov Danila <ohstapit@gmail.com>
2021-12-25 13:15:11 +0300

    Scripted auto-commit on change (2021-12-25 13:15:11) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/akka/ZookeeperController.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/akka/ZookeeperController.java
@@ -19,7 +19,7 @@ public class ZookeeperController {
 
     public ZookeeperController(String host, ActorRef actor) {
         this.actor = actor;
-        this.zoo 
+        this.zoo = new ZooKeeper()
     }
 
     public void close() throws InterruptedException {
